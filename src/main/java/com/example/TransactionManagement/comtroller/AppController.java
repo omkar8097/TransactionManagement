2a046eb2a221f949e96b51185f2229cac65c0796
omkar8097 <komkar8.ok@gmail.com>
2022-12-27 10:28:47 +0530

    first

---
+++ src/main/java/com/example/TransactionManagement/comtroller/AppController.java
@@ -0,0 +1,12 @@
+package main.java.com.example.TransactionManagement.comtroller;
+
+
+
+
+public class AppController {
+
+    public void get() {
+        System.out.println("get api calleds");
+    }
+    
+}
